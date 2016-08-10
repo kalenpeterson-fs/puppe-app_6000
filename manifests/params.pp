885999111ab95110b1ff5324e2a2d8abd93ba4dd
Kalen Peterson <kpeterson@forsythe.com>
2016-08-10 09:44:38 -0500

    Remove old MCO code

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,15 +4,12 @@ class app_6000::params {
 
   case $::osfamily {
       'RedHat': {
-        $mcoLibDir = '/opt/puppetlabs/mcollective/plugins'
         $provider  = 'yum'
       }
       'Debian': {
-        $mcoLibDir = '/opt/puppetlabs/mcollective/plugins'
         $provider  = 'apt'
       }
       'windows': {
-        $mcoLibDir = "${::env_windows_installdir}/mcollective/lib"
         $provider  = 'chocolatey'
       }
       default: {
